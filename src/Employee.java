0aea52ba4140b0acdb2ba2fd9a6fa0fd07af53a2
narawan.i <narawan.i@kbtg.tech>
2023-03-13 14:48:33 +0700

    Working with constructor

--- src/Employee.java
+++ src/Employee.java
@@ -1,10 +1,26 @@
 public class Employee {
+    // Properties or states
     int id;
     String fname;
     String lname;
     String pos;
 
+    //Constructor
+    public Employee() {
+//        this.fname = "";
+//        this.lname = "";
+//        ย่้ายการทำงานไปอยู่ที่เดียว เพราะการทำงานเหมือนกัน
+        this("?", "?");
+    }
+
+    // overload Constructor ชื่อเหมือนกัน แต่ parameter ต่างกัน ถ้ามี แล้ว default Constructor จะหายไป ต้องสร้าง default Constructor เอง
+    public Employee(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    // Behaviors or methods
     String getFullName() {
-        return fname + " " +lname;
+        return fname + " " + lname;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Employee narawan = new Employee();
+        // default Constructor
+        Employee test = new Employee();
+
+        String resultTest = test.getFullName();
+        System.out.println(resultTest);
+
+        // Constructor
+        Employee narawan = new Employee("Narawan", "Iamsaard");
+
         String result = narawan.getFullName();
         System.out.println(result);
     }
