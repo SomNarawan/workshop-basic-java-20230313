80f7bee2f4de18dc1310c77aae1abca416acfddf
narawan.i <narawan.i@kbtg.tech>
2023-03-20 19:46:42 +0700

    First case with empty list

--- src/MySort.java
+++ src/MySort.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySort {
+    public List<Integer> sort(List<Integer> inputs) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
 }
--- src/MySortTest.java
+++ src/MySortTest.java
@@ -1,5 +1,23 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySortTest {
+
+    @Test
+    @DisplayName("Empty list should be sorted")
+    public void caseEmpty(){
+        List<Integer> expectResult = new ArrayList<>();
+
+        MySort mySort = new MySort();
+        List<Integer> inputs = Arrays.asList();
+        List<Integer> actualResult = mySort.sort(inputs);
+        assertEquals(expectResult, actualResult);
+    }
 
 }
