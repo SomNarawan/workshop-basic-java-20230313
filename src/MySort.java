086eec49932b15d679385a05a0387840b7b0d06d
narawan.i <narawan.i@kbtg.tech>
2023-03-20 19:49:26 +0700

    case [1,2] => [1,2]

--- src/MySort.java
+++ src/MySort.java
@@ -4,6 +4,6 @@ import java.util.List;
 public class MySort {
     public List<Integer> sort(List<Integer> inputs) {
         List<Integer> result = new ArrayList<>();
-        return result;
+        return inputs;
     }
 }
--- src/MySortTest.java
+++ src/MySortTest.java
@@ -11,11 +11,22 @@ class MySortTest {
 
     @Test
     @DisplayName("Empty list should be sorted")
-    public void caseEmpty(){
-        List<Integer> expectResult = new ArrayList<>();
+    public void case01(){
+        List<Integer> expectResult = Arrays.asList();
 
         MySort mySort = new MySort();
         List<Integer> inputs = Arrays.asList();
+        List<Integer> actualResult = mySort.sort(inputs);
+        assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("[1,2] => [1,2]")
+    public void case02(){
+        List<Integer> expectResult = Arrays.asList(1,2);
+
+        MySort mySort = new MySort();
+        List<Integer> inputs = Arrays.asList(1,2);
         List<Integer> actualResult = mySort.sort(inputs);
         assertEquals(expectResult, actualResult);
     }
